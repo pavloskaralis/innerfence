9bac05166df30d48bf8548d6434d449f27865044
PAVLOS KARALIS <PAVLOSKARALIS@gmail.com>
2020-10-20 18:24:49 -0400

    merge

--- ios/RNInnerfence/RNInnerfence.m
+++ ios/RNInnerfence/RNInnerfence.m
@@ -14,42 +14,13 @@
 #import "../innerfence/IFChargeRequest.h"
 #import "../innerfence/IFChargeResponse.h"
 
-@interface RNInnerfence ()
-
-@end
-
 @implementation RNInnerfence
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+RCT_EXPORT_MODULE();
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (IBAction)chargeTapped:(id)sender {
-    IFChargeRequest* chargeRequest = [IFChargeRequest new];
-
-    // Include my record_id so it comes back with the response
-    [chargeRequest
-        setReturnURL:@"com-innerfence-ChargeDemo://chargeResponse"
-        withExtraParams:@{ @"record_id": @"123" }
-    ];
-
-    chargeRequest.amount        = @"50.00";
-    chargeRequest.description   = @"Test transaction";
-    chargeRequest.invoiceNumber = @"321";
-
-    // Include a tax rate if you want Credit Card terminal to calculate
-    // sales tax. If you pass in @"default", we'll use the default sales
-    // tax preset by the user. If you leave it as nil, we’ll hide the
-    // sales tax option from the user.
-    chargeRequest.taxRate = @"8.5";
-
-    [chargeRequest submit];
+RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
+{
+  RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
 }
 
 @end
